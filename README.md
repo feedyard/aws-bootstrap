2cc65e976ec5ec80df85ab982b79d995d2235cc3
Nic Cheneweth <nchenewe@thoughtworks.com>
2017-12-30 17:22:46 -0600

    action complete

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# bootstrap-aws
-Greenfield assumptions and minimal configuration
+# feedyard/bootstrap-aws
+Starting with a newly created AWS Account, a minimal bootstrap configuration must occur to support an IaC process
+for managing aws state.  
+
+With bootstrap access available, and assuming the configuration tools (terraform, kops, etc) maintain aws state files,
+then the only bootstrap setup really needed is S3 buckets for state information. (see below for additional bootstrap
+config for self managed pipelines.)
+
+By default the buckets will be created with the following names:  
+<project or org name parameter>-<account name>-state  
+
+The project or org name is passed to the create command:  
+`$ invoke create test-project`
+
+_note_: routine s3, unique naming requirements apply.  
+
+### Requirements
+
+A bootstrap group/user has been created in each account with permissions sufficient to execute terraform
+configuration plans, and this access information is maintained locally in standard ~/.aws configuration files.  
+
+Create a local file _accounts.json_ in the following format. The tasks.py file will use this information to setup the
+appropriate profile information for account access.  
+
+```json
+{
+  "platform": "platform or project name to use in bucket name",
+  "accounts": {
+    "account1 name": "matching aws profile name in ~/.aws/credentials",
+    "account2 name": "matching aws profile name in ~/.aws/credentials",
+    "account3 name": "matching aws profile name in ~/.aws/credentials",
+    "account4 name": "matching aws profile name in ~/.aws/credentials"
+  }
+}
+```
+
+python  
+  pkg: invoke, boto3
+
+
+#### external pattern
+The external pattern assumes the use of a pipeline orchestration tool already available and maintained separately from
+the infrastructure team that owns this platform product. CircleCi.com is an example of a vendor provided product, or it
+could be something self-managed elsewhere in the enterprise but available/appropriate to the needs of this team.
+
+#### internal pattern
+Where the bootstrap process requires aws based compute from which to run a pipeline tool, the /internal folder includes
+terraform to support such a self-managed environment in the production account.
+
+
+folder with docker-swarm/jenkins pipeline
---
+++ tasks.py
@@ -0,0 +1,118 @@
+from invoke import task
+from pathlib import Path
+import sys
+import boto3, botocore
+import json
+
+accounts_file = Path('./accounts.json')
+platform_name = ''
+account_profiles = ''
+
+# uses project or Org name as first word followed by account name
+# example: feedyard-sandbox-state
+bucket_name = '{}-{}-state'
+
+print ('CLI: this s3 bucket creation script is intended to be used as part of a new AWS account bootstrap event and ' \
+       'is therefore not expected to be used past the this initial kickoff step.\n')
+
+@task
+def test(ctx):
+    load_profiles()
+    accounts = account_profiles['accounts']
+    for acct in accounts:
+        print(accounts[acct])
+
+@task
+def list(ctx):
+    global accounts_file
+    global account_profiles
+
+    load_profiles()
+    for profile in account_profiles:
+        print('Current buckets in account:{}'.format(profile))
+        session = boto3.Session(profile_name=account_profiles[profile])
+        s3 = session.client('s3')
+        response = s3.list_buckets()
+        buckets = [bucket['Name'] for bucket in response['Buckets']]
+        print("\t%s" % buckets)
+
+@task
+def create(ctx):
+    global accounts_file
+    global platform_name
+    global account_profiles
+    global bucket_name
+    global session
+
+    load_profiles()
+    for profile in account_profiles:
+        bucket = bucket_name.format(platform_name, profile)
+        print('Create state s3 bucket {} in account:{}'.format(bucket, profile))
+        session = boto3.Session(profile_name=account_profiles[profile])
+        if bucket_does_not_exist(bucket):
+            # create bucket
+            s3 = session.client('s3')
+            s3.create_bucket(Bucket=bucket)
+            # enable bucket versioning
+            s3 = session.resource('s3')
+            bucket_versioning = s3.BucketVersioning(bucket)
+            bucket_versioning.enable()
+
+        else:
+            print ('{} already exists')
+
+@task
+def delete(ctx):
+    global accounts_file
+    global platform_name
+    global account_profiles
+    global bucket_name
+    global session
+
+    response = input('This will destroy all the bootstrap state buckets. You must respond with YES to proceed ' \
+                     '\nAre you sure?')
+
+    if response == 'YES':
+        load_profiles()
+        for profile in account_profiles:
+            del_bucket = bucket_name.format(platform_name, profile)
+            print('Delete s3 bucket {} in account:{}'.format(del_bucket, profile))
+            session = boto3.Session(profile_name=account_profiles[profile])
+            if bucket_does_not_exist(del_bucket):
+                print ('Cannot delete {}, does not exists')
+            else:
+                s3 = session.resource('s3')
+                bucket = s3.Bucket(del_bucket)
+                for key in bucket.objects.all():
+                    key.delete()
+                bucket.delete()
+
+def load_profiles():
+    global accounts_file
+    global platform_name
+    global account_profiles
+
+    if accounts_file.is_file():
+        file_contents = json.loads(open(accounts_file).read())
+        platform_name = file_contents['platform']
+        account_profiles = file_contents['accounts']
+    else:
+        print ('{} does not exists'.format(accounts_file))
+        sys.exit(1)
+
+def bucket_does_not_exist(bucket):
+    global session
+
+    s3 = session.resource('s3')
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket)
+        return False
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = int(e.response['Error']['Code'])
+        if error_code == 403:
+            print("Private Bucket. Forbidden Access!")
+            return False
+        elif error_code == 404:
+            return True
