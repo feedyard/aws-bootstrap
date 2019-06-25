f787b8829308a5e81365eedbb3472e3764940310
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-06-24 23:48:24 -0500

    iterating

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,9 +2,9 @@
 from invoke import Collection
 from tasks import deploy
 from tasks import delete
-from tasks import validate
+from tasks import gpg
 
 ns = Collection()
 ns.add_collection(deploy)
 ns.add_collection(delete)
-ns.add_collection(validate)
+ns.add_collection(gpg)
