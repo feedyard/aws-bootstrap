9af5f8f323552f11dc2b51141c6f65e54096273b
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-06-27 15:38:04 -0700

    iterating

--- key-value-store/main.tf
+++ key-value-store/main.tf
@@ -28,21 +28,17 @@ resource "aws_s3_bucket_policy" "key-value-store-bucket-policy" {
   policy = <<POLICY
 {
   "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "${local.current-account-arn}",
-      },
-      "Action": [
+  "Statement": [{
+    "Effect": "Allow",
+    "Principal": { "AWS": "${local.current-account-arn}" },
+    "Action": [
         "s3:ListBucket",
         "s3:GetObject",
         "s3:PutObject",
         "s3:DeleteObject"
       ],
-      "Resource": "arn:aws:s3:::${local.bucket-name}",
-    }
-  ]
+    "Resource": "arn:aws:s3:::${local.bucket-name}"
+  }]
 }
 POLICY
 }
