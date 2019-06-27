d13064f888e7f5a7fabea3ab3060200b3dac5e99
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-06-27 14:47:33 -0700

    iterating

--- key-value-store/main.tf
+++ key-value-store/main.tf
@@ -2,26 +2,26 @@
 resource "aws_s3_bucket" "key-value-store" {
   bucket = "${local.bucket-name}"
   acl    = "private"
-  policy = <<EOF
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Effect":"Allow",
-      "Principal": { "AWS": ["${local.current-account-arn}"] },
-      "Action": [
-        "s3:ListBucket",
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject"
-      ]
-      "Resource": [
-        "arn:aws:s3:::${local.bucket-name}"
-      ]
-    }
-  ]
-}
-EOF
+//  policy = <<EOF
+//{
+//  "Version": "2012-10-17",
+//  "Statement": [
+//    {
+//      "Effect":"Allow",
+//      "Principal": { "AWS": ["${local.current-account-arn}"] },
+//      "Action": [
+//        "s3:ListBucket",
+//        "s3:GetObject",
+//        "s3:PutObject",
+//        "s3:DeleteObject"
+//      ]
+//      "Resource": [
+//        "arn:aws:s3:::${local.bucket-name}"
+//      ]
+//    }
+//  ]
+//}
+//EOF
 
   versioning {
     enabled = "true"
